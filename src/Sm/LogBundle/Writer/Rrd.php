276ec6a48fe3210c5d3535055be25996ea423da2
Joris Steyn <joris@ibuildings.nl>
2014-06-26 21:35:25 +0200

    Generate better rrd graph

--- src/Sm/LogBundle/Writer/Rrd.php
+++ src/Sm/LogBundle/Writer/Rrd.php
@@ -15,21 +15,24 @@ class Rrd
         }
 
         rrd_update($this->rrdFile, [
-            '-t powerin',
             $data->timestamp->format('U') . ':' .
-            round($data->current_power_in * 100)
+            $data->current_power_in
         ]);
-        var_dump(
-            $data->timestamp->format('U') . ':' .
-            round($data->current_power_in * 100)
-        );
+        var_dump(rrd_error());
+
         rrd_graph(dirname($this->rrdFile) . '/test.png', [
+            "--width", "800",
+            "--height", "200",
             "--vertical-label", "kW",
-            "--lower", "0",
-            "--start", "-1 hour",
-            "DEF:bla={$this->rrdFile}:powerin:AVERAGE",
-            "CDEF:realpower=bla,1000,*",
-            "LINE2:realpower#FF0000"
+            "--lower", "0.3",
+            "--start", "-10 minutes",
+            "DEF:realpower={$this->rrdFile}:powerin:AVERAGE",
+            //            "CDEF:realpower=bla,1000,*",
+            //"LINE1:realpower#FF0000:powerin\\r",
+            "LINE1:realpower#0000FF:powerin\\r",
+            "GPRINT:realpower:MIN:Min kW\: %4.2lf",
+            "GPRINT:realpower:AVERAGE:Avg kW\: %4.2lf",
+            "GPRINT:realpower:MAX:Max kW\: %4.2lf",
         ]);
         var_dump(rrd_error());
     }
@@ -38,8 +41,10 @@ class Rrd
     {
         rrd_create($this->rrdFile, [
             "--step", "10",
-            "DS:powerin:ABSOLUTE:60:0:100",
+            "DS:powerin:GAUGE:60:0:1",
             "RRA:AVERAGE:0.5:1:360",
+            "RRA:MIN:0.5:1:360",
+            "RRA:MAX:0.5:1:360",
         ]);
         var_dump(rrd_error());
     }
